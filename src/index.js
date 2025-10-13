423f7b436a6610211d46a394976ff7cce84fef59
Arkajyoti Kundu <arkajyotikundu415@gmail.com>
2025-10-13 23:59:51 +0530

    add servicesworker to make pwa app

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import * as serviceWorkerRegistration from './Pwa/serviceWorkerRegistration';
+
+serviceWorkerRegistration.register();
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
